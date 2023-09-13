a868c5a4ac2d744b7fc3e27854dd933b2a0c3703
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-12 19:31:14 -0600

    create skills data and update skills view to match new data

---
+++ data/skills.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'HTML', learned: true, _id: 1111},
+  {text: 'CSS', learned: true, _id: 2222},
+  {text: 'JavaScript', learned: true, _id: 3333},
+  {text: 'Express', learned: false, _id: 4444},
+  {text: 'MongoDB', learned: false, _id: 5555},
+]
+
+export { skills }
--- routes/skills.js
+++ routes/skills.js
@@ -1,11 +1,18 @@
 import { Router } from 'express'
+import { skills } from '../data/skills.js'
+// import { app } from '../server'
 
 const router = Router()
 
-// GET localhost:3000/skills
 router.get('/', (req, res) => {
-  res.render('skills', { title: 'Developer Skills'})
+  res.render('skills/index.ejs', {
+    skills: skills
+  })
 })
 
-
 export { router }
+
+// GET localhost:3000/skills
+// router.get('/', (req, res) => {
+//   res.render('skills', { title: 'Developer Skills'})
+// })
